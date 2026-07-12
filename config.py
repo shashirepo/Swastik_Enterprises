5340519ecee73a39e0d7f3f9b4021694c38aa639
shashirepo <37866648+shashirepo@users.noreply.github.com>
2026-07-12 20:03:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,13 +22,13 @@ GST_OPTIONS  = [0.0, 5.0, 12.0, 18.0, 28.0]
 COMMON_UNITS = ["Pcs.", "MTR", "KG", "Set", "Pair", "Bag", "Box", "Roll", "Ltr", "Nos.", "Mtr"]
 
 SAMPLE_ITEMS = [
-    ("ADANI SOLAR PANEL 620 WATT N-TYPE TOPCON", "85414300",  5.0,  "Pcs.", 22000.00, "ADANI",   5.0),
+    ("SOLAR PANEL 620 WATT N-TYPE TOPCON", "85414300",  5.0,  "Pcs.", 22000.00, "ADANI",   5.0),
     ("ON GRID INVERTER 3.3 KW ",             "85044090",  1.0,  "Pcs.", 19500.00, "DEYE",   18.0),
     ("3KW APOLLO SOLAR STRUCTURE (MEDIUM 4FT X 6FT)",        "73089030",  1.0,  "Set",  13000.00, "GENERIC",18.0),
     ("ACDB & DCDB BOX",                           "85369030",  1.0,  "Set",      0.00, "GENERIC",18.0),
-    ("POLYCAB DC WIRE 4MM",                      "85446090", 60.0,  "MTR",      0.00, "POLYCAB",18.0),
-    ("POLYCAB AC WIRE 4MM",                      "85446090", 90.0,  "MTR",      0.00, "POLYCAB",18.0),
-    ("EARTHING COPPER WIRE 4MM",                        "85446026", 90.0,  "MTR",      0.00, "GENERIC",18.0),
-    ("INSTALLATION CHARGE",                        "85379995",  1.0,  "Set",      0.00, "",       18.0),
-    ("TRANSPORT CHARGE",                           "85389965",  1.0,  "Set",      0.00, "",       18.0),
+    ("POLYCAB DC WIRE 4MM",                      "85446090", 40.0,  "MTR",      0.00, "POLYCAB",18.0),
+    ("POLYCAB AC WIRE 4MM",                      "85446090", 45.0,  "MTR",      0.00, "POLYCAB",18.0),
+    ("EARTHING COPPER WIRE 4MM",                        "85446026", 70.0,  "MTR",      0.00, "GENERIC",18.0),
+    ("INSTALLATION CHARGE",                        "85379995",  1.0,  "Set",      8000.00, "",       18.0),
+    ("TRANSPORT CHARGE",                           "85389965",  1.0,  "Set",      2000.00, "",       18.0),
 ]
